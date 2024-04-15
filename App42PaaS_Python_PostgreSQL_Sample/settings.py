8a87c9a083a09007513a086062717bc8e7368a96
Venkat-Narayan-07 <137889652+Venkat-Narayan-07@users.noreply.github.com>
2024-04-15 10:48:27 +0530

    Update settings.py

--- App42PaaS_Python_PostgreSQL_Sample/settings.py
+++ App42PaaS_Python_PostgreSQL_Sample/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'NAME': 'azdeveloperprojectapp-server',                      # Or path to database file if using sqlite3.
         'USER': 'kxlzhuyseg',                      # Not used with sqlite3.
         'PASSWORD': 'm4mNIRST8R3yy$gn',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '20.119.8.51',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 		'OPTIONS': {
             'sslmode': 'allow',
